c702629cf63f4d3c967a010848354a213c98a1ba
Nguyen Minh Nhat <minhtientiny@gmail.com>
2025-05-31 09:42:00 +0700

    update linh tinh

--- scripts/fuckdevtools.js
+++ scripts/fuckdevtools.js
@@ -1,4 +1,3 @@
-// Thiết lập chặn các hành vi DevTools
 const disable_right_click = true;
 const disable_f12 = true;
 const disable_csi = true;
@@ -8,7 +7,7 @@ const disable_ctrl_u = true;
 document.addEventListener('contextmenu', function (e) {
     if (disable_right_click) {
         e.preventDefault();
-        toastr.error("DevTools?");
+        FuiToast.error("DevTools?")
     }
 });
 
@@ -16,28 +15,28 @@ document.addEventListener('keydown', function (e) {
     // F12
     if (disable_f12 && e.keyCode === 123) {
         e.preventDefault();
-        toastr.error("F12 bị chặn!");
+        FuiToast.error("F12 bị chặn!")
         return false;
     }
 
     // Ctrl + Shift + I
     if (disable_csi && e.ctrlKey && e.shiftKey && e.keyCode === 73) {
         e.preventDefault();
-        toastr.error("Ctrl+Shift+I bị chặn!");
+        FuiToast.error("Ctrl+Shift+I bị chặn!")
         return false;
     }
 
     // Ctrl + Shift + J
     if (disable_cs_j && e.ctrlKey && e.shiftKey && e.keyCode === 74) {
         e.preventDefault();
-        toastr.error("Ctrl+Shift+J bị chặn!");
+        FuiToast.error("Ctrl+Shift+J bị chặn!")
         return false;
     }
 
     // Ctrl + U
     if (disable_ctrl_u && e.ctrlKey && e.keyCode === 85) {
         e.preventDefault();
-        toastr.error("Ctrl+U bị chặn!");
+        FuiToast.error("Ctrl+U bị chặn!")
         return false;
     }
 });
